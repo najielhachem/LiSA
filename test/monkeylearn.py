d1e24ba2836ebd1d6a84a2e145b16d45598a8704
nagielhachem <nagielhachem@gmail.com>
2017-09-26 11:01:09 +0200

    rearranging files

--- test/monkeylearn.py
+++
@@ -1,7 +0,0 @@
-from monkeylearn import MonkeyLearn
-
-ml = MonkeyLearn('04aee1a0ed2a52800a7e593d9ef14d26c86b2ca4')
-text_list = ["yooow!!"]
-module_id = 'cl_qkjxv9Ly'
-res = ml.classifiers.classify(module_id, text_list, sandbox=False)
-print(res.result)
