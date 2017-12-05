63f78e90f4f0d2dcb4aa39a1a8bbb49fd3010f4f
nagielhachem <nagielhachem@gmail.com>
2017-12-05 15:51:56 +0100

    docstring added to parser.query

--- src/data-processing/parser.py
+++ src/data-processing/parser.py
@@ -1,7 +1,20 @@
 from twitterscraper import query_tweets
 
 def query(subject, since, until, near = None, limit = None):
-    query = subject
+    """
+    Returns tweets matching parameters
+
+    :param subject: subject of tweet ("Trump OR Nagi") 
+    :param since: fetch tweets since that date ("2017-11-30")
+    :param until: fetch tweets until that date ("2017-12-24")
+    :param near: fetch tweets published near that location ("New York")
+    :param limit: minimum number of tweets to fetch ("1000")
+
+    :return tweets: tweets that match description
+    :rtype: list<Tweet>
+    """
+    query = ""
+    query += subject
     if near is not None:
         query += " near:" + near
     query += " since:" + since
@@ -10,6 +23,11 @@ def query(subject, since, until, near = None, limit = None):
     return query_tweets(query, limit)
         
 def save_query(filename, subject, since, until, near = None, limit = None):
+    """
+    Fetch tweets matching description and save them into file
+
+    
+    """
     file = open(filename, "w")
     for tweet in query(subject, since, until, near, limit):
         file.write(tweet.encode('utf-8'))
