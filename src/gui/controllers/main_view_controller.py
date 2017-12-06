66c15e486a321d96d50919ea9be5c47e1a3b7206
nagielhachem <nagielhachem@gmail.com>
2017-12-06 19:03:55 +0100

    main_view_controller compiles

--- src/gui/controllers/main_view_controller.py
+++ src/gui/controllers/main_view_controller.py
@@ -1,10 +1,9 @@
-
+from controller import Controller
 
 class MainViewController(Controller):
 
     def __init__(self, view):
         self.view = view
 
-
     def fetch(self):
-        parser(view.subject)
+        pass
