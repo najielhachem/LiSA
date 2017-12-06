115ba9c2a3f530c2bbd2dec98d169cf1f11b9ff1
Senhaji-Rhazi-Hamza <hamza.senhaji-rhazi@epita.fr>
2017-12-06 18:12:54 +0100

    add interface.py + Datepicker.py

---
+++ src/gui/views/interface.py
@@ -0,0 +1,36 @@
+from tkinter import *
+
+import calendar
+import datetime
+from tkcalendar import Calendar
+import DatePicker as dp
+
+root = Tk()
+root.title('Lisa')
+root['bg']='white'
+
+
+# frame 1
+inputFrame = Frame(root, borderwidth=2)
+inputFrame.pack(side=TOP, padx=10, pady=10)
+
+for line, item in enumerate(['input', 'suject', 'Location']):
+    l = Label(inputFrame, text=item, width=10)
+    e = Entry(inputFrame, width=10)
+    l.grid(row=line, column=0)
+    e.grid(row=line, column=1)
+Label(inputFrame, text='DatePicker', width=10).grid(row=1, column = 2)
+
+dp.Datepicker(inputFrame).grid(row = 0, column = 3)
+dp.Datepicker(inputFrame).grid(row = 2, column = 3)
+
+
+OptionFrame = Frame(root, borderwidth=2, relief=GROOVE)
+OptionFrame.pack(side=TOP, padx=10, pady=10)
+
+PlotFrame = Frame(root, borderwidth=2, relief=GROOVE)
+PlotFrame.pack(side=TOP, padx=10, pady=10)
+
+
+
+root.mainloop()
