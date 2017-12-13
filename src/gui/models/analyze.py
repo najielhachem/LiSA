2800cf97414c7b93647a460975de3552e8a6306a
Antoine Corbel <corbel.antoine@gmail.com>
2017-12-13 20:09:57 +0100

    Improve analyzer

--- src/gui/models/analyze.py
+++ src/gui/models/analyze.py
@@ -1,9 +1,12 @@
-
+from classifiers import *
 
 class Analyzer:
-    def __init__(self, classifieur = Null):
-        pass
+    def __init__(self, classifier = BernoulliNB()):
+        self.classifier = classifier
 
-    def analyze(self, tweet_text):
-        """ Return, using defined classifier, value between 0 and 1"""
-        pass
+    def analyze(self, tweet_texts):
+        """ Return, using defined classifier, value between 0 and 1
+            Args:
+                tweet_texts ([string]): Array of string corresponding to tweets
+        """
+        return classifier.predict(tweet_texts)
