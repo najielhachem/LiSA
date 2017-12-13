8550bc318b77ad1b9a0252480aea8e86e222b393
Antoine Corbel <corbel.antoine@gmail.com>
2017-12-13 18:13:53 +0100

    Add class analyze

---
+++ src/gui/models/analyze.py
@@ -0,0 +1,9 @@
+
+
+class Analyzer:
+    def __init__(self, classifieur = Null):
+        pass
+
+    def analyze(self, tweet_text):
+        """ Return, using defined classifier, value between 0 and 1"""
+        pass
