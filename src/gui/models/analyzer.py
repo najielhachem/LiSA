433f7ab713ea2d6985e0f42d26c63d1ae70c391d
nagielhachem <nagielhachem@gmail.com>
2017-12-14 18:21:27 +0100

    segment_tweets function moved from Parser to Analyzer

--- src/gui/models/analyzer.py
+++ src/gui/models/analyzer.py
@@ -13,3 +13,17 @@ class Analyzer:
                 tweet_texts ([string]): Array of string corresponding to tweets
         """
         return self.classifier.predict(self.tweets)
+
+    def segment_tweets(tweets, nb_segments):
+        """
+            Return list of tweets segmented by the attribute timestamp on [nb_segments - 1] interval
+        Params :
+            :tweets -- list[tweet]: list of tweets done by a query
+        """
+        time_stamps = np.array([tweet[i].__getattribute__('timestamp') for i in range(len(tweets))])
+        time_min = time_stamps.min()
+        time_max = time_stamps.max()
+        delta = datetime.timedelta(seconds = ((time_max - time_min).total_seconds()/ nb_segments))
+        segemented_tweets = [[t for t in tweets
+        if( (( i  * delta) + time_min ) <= t.__getattribute__('timestamp') < (( (i + 1) * delta) + time_min ) )] for i in range(nb_segments)]
+        return segemented_tweets
