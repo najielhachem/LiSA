5fd980ab334258a268da49be1da6f10139f595db
Senhaji-Rhazi-Hamza <hamza.senhaji-rhazi@epita.fr>
2018-01-24 17:43:04 +0100

    add install.sh file

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install python-tk python-imaging-tk python3-tk
+sudo pip3 install -U nltk
+sudo pip3 install -U numpy
+sudo pip3 install simplejson
+sudo pip3 install twitterscraper==0.3.3
